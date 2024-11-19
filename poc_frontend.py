ab2296f048e83f8a5d2465b26450464f91cade40
nihar2121 <114141013+nihar2121@users.noreply.github.com>
2024-11-19 12:28:27 +0530

    Add files via upload

--- poc_frontend.py
+++ poc_frontend.py
@@ -8,7 +8,7 @@ import extract_msg  # To read .msg files
 from bs4 import BeautifulSoup
 import openpyxl
 # Importing the backend functions
-from poc_backend import process_star_india_diachi,process_future_generalli_life_insurance,process_aditya_insurance_co,process_manipal_health_insurance_company,process_generali_india_insurance_company,process_magma_hdi_general_insurance_company,process_care_health_insurance_limited,process_bajaj_allianz_life_insurance,process_relaince_general_insurance_co, process_hdfc_ergo_insurance, process_bajaj_allianz_insurance,process_tata_aig_insurance,process_royal_sundaram_general_insurance,process_raheja_general_insurance,process_godigit_general_insurance,proess_acko_general_insurance,process_sbi_general_insurance,process_liberty_general_insurance,process_cholamandalam_general_insurance,process_icici_prudential_life_insurance,process_zuna_general_insurance, process_universal_sampo_insurance,process_kotak_mahindra_insurance,process_shriram_general_insurance,process_hdfc_life_insurance_co,process_star_health_insurer,read_lookup_files,process_icici_lombard_insurance, process_new_india_assurance,process_oriental_insurance_co, process_united_india_insurance,process_tata_aia_insurance
+from poc_backend import process_sbi_life_insurance_co,process_iffco_tokyo_insurer,process_star_india_diachi,process_future_generalli_life_insurance,process_aditya_insurance_co,process_manipal_health_insurance_company,process_generali_india_insurance_company,process_magma_hdi_general_insurance_company,process_care_health_insurance_limited,process_bajaj_allianz_life_insurance,process_relaince_general_insurance_co, process_hdfc_ergo_insurance, process_bajaj_allianz_insurance,process_tata_aig_insurance,process_royal_sundaram_general_insurance,process_raheja_general_insurance,process_godigit_general_insurance,proess_acko_general_insurance,process_sbi_general_insurance,process_liberty_general_insurance,process_cholamandalam_general_insurance,process_icici_prudential_life_insurance,process_zuna_general_insurance, process_universal_sampo_insurance,process_kotak_mahindra_insurance,process_shriram_general_insurance,process_hdfc_life_insurance_co,process_star_health_insurer,read_lookup_files,process_icici_lombard_insurance, process_new_india_assurance,process_oriental_insurance_co, process_united_india_insurance,process_tata_aia_insurance
 from datetime import datetime
 from urllib.parse import unquote_plus
 
@@ -1018,7 +1018,40 @@ def select_insurer():
                 'Premium': 'Premium',
                 'Commisison': 'Brokerage'
                                                       }
-                
+        elif selected_insurer == 'SBI Life Insurance Company Limited':
+
+            read_tables_from_email(email_body, selected_insurer)  # Pass the selected insurer to the function
+            default_mappings = {
+                'Product Name': 'Risk',
+                'Policy Number': 'Policy No.',
+                'Policy Holder': 'Client Name',
+                'POLICY_END_DATE': 'Policy End Date',
+                'Risk Date': 'Policy Start Date',
+                'Policy Holder Permenant State': 'Branch',
+                'Premium Type ': 'Income Category',
+                'Endoresement': 'Endorsement No.',
+                'Premium': 'Premium',
+                'Payable Gross commission': 'Brokerage'
+                                                      }
+        elif selected_insurer == 'IFFCO TOKIO General Insurance Co. Ltd.':
+
+            read_tables_from_email(email_body, selected_insurer)  # Pass the selected insurer to the function
+            default_mappings = {
+                'Product Name': 'Risk',
+                'Policy number': 'Policy No.',
+                'Client Name': 'Client Name',
+                'Risk exp date': 'Policy End Date',
+                'Risk inc date': 'Policy Start Date',
+                'Geographical State': 'Branch',
+                'Type of Transaction': 'Income Category',
+                'Endorsement ind': 'Endorsement No.',
+                'Gross Premium': 'Premium1',
+                'Terrorism premium': 'Premium2',
+                'Tp premium': 'Premium3',
+                'Base Commission': 'Brokerage1',
+                'Comm/Brokerage Amount(OD)': 'Brokerage2',
+                'Comm/Brokerage Amount(TP/Terr)': 'Brokerage3'
+                                                      }                
         else:
             # Ensure there's a fallback for other insurers
             default_mappings = {
@@ -1072,7 +1105,9 @@ def edit_mappings():
         'Manipal Cigna Health Insurance Company Limited (Prev. Cigna Ttk Health Insurance':0,
         'Aditya Birla Health Insurance Co.Ltd.':0,
         'Future Generali India Life Insurance Co Ltd':0,
-        'Star Union Dai-Ichi Life Insurance Company Ltd':0
+        'Star Union Dai-Ichi Life Insurance Company Ltd':0,
+        'IFFCO TOKIO General Insurance Co. Ltd.':0,
+        'SBI Life Insurance Company Limited':0
         # Add more insurers as needed
     }
 
@@ -1258,7 +1293,7 @@ def read_tables_from_email(email_body, selected_insurer):
         'ICICI Lombard General Insurance Co. Ltd.', 'Star Health Insurance', 'Hdfc Life Insurance Co. Ltd.',
         'Shriram General Insurance Company Limited', 'Kotak Mahindra General Insurance Company',
         'Universal Sampo Insurance', 'Zuno General Insurance Limited', 'ICICI Prudential Life Insurance Co Ltd',
-        'Star Union Dai-Ichi Life Insurance Company Ltd','Future Generali India Life Insurance Co Ltd','Aditya Birla Health Insurance Co.Ltd.','Manipal Cigna Health Insurance Company Limited (Prev. Cigna Ttk Health Insurance','Future Generali India Insurance Company Limited','Magma Hdi General Insurance Company Limited','Care Health Insurance Limited','Bajaj Allianz Life Insurance Company Limited','Reliance General Insurance Co. Ltd.','Hdfc Ergo General Insurance Company Limited','Bajaj Allianz General Insurance Co. Ltd.','Tata AIG General Insurance Co. Ltd.','Royal Sundaram General Insurance Co Ltd','Raheja Qbe General Insurance Company Limited','GoDigit General Insurance Limited','Acko General Insurance Limited','SBI General Insurance Company Limited','Cholamandalam General Insurance Co. Ltd.', 'Tata AIA Insurance','Liberty Videocon General Insurance Co. Ltd'
+        'SBI Life Insurance Company Limited','IFFCO TOKIO General Insurance Co. Ltd.','Star Union Dai-Ichi Life Insurance Company Ltd','Future Generali India Life Insurance Co Ltd','Aditya Birla Health Insurance Co.Ltd.','Manipal Cigna Health Insurance Company Limited (Prev. Cigna Ttk Health Insurance','Future Generali India Insurance Company Limited','Magma Hdi General Insurance Company Limited','Care Health Insurance Limited','Bajaj Allianz Life Insurance Company Limited','Reliance General Insurance Co. Ltd.','Hdfc Ergo General Insurance Company Limited','Bajaj Allianz General Insurance Co. Ltd.','Tata AIG General Insurance Co. Ltd.','Royal Sundaram General Insurance Co Ltd','Raheja Qbe General Insurance Company Limited','GoDigit General Insurance Limited','Acko General Insurance Limited','SBI General Insurance Company Limited','Cholamandalam General Insurance Co. Ltd.', 'Tata AIA Insurance','Liberty Videocon General Insurance Co. Ltd'
     ]
 
     if selected_insurer not in supported_insurers:
